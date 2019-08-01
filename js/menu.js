ffc37dd551e53f32c02023e17e32aeb30f3317c7
KhanhNCK <khanhcong696@gmail.com>
2019-08-01 23:34:14 +0700

    add file BTVN 2

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+$('#carouselId').carousel({
+    interval: 3000,
+    cycle: true
+  }); 
+
+  // hover ảnh
+  /* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
